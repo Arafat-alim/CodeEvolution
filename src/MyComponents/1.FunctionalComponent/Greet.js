cf75610dd930c95fb3e45689754284a327c64da0
Arafat <arafat97alim@gmail.com>
2021-07-14 23:13:50 +0530

    added functional Component.

---
+++ src/MyComponents/1.FunctionalComponent/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//old tradition way , try to used arrow function
+// function Greet() {
+//   return <h1>Hello Arafat</h1>;
+// }
+
+//Arrow Function
+export let Greet = () => <h1>Hello Miya</h1>; //single line function best to use it
+
+// export default Greet; //default component export
