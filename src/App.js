b217d39c91eb3e6174cdcd53a2727e267faae6e5
Arafat <arafat97alim@gmail.com>
2021-07-15 00:13:10 +0530

    Added class component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+// import Welcome from "./MyComponents/2.ClassComponent/Welcome";
 import { Greet } from "./MyComponents/FunctionalComponent/Greet";
+import Home from "./MyComponents/ClassComponent/Home";
+
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Home />
     </div>
   );
 }
