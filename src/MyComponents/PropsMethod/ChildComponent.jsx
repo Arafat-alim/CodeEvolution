63177517901283ea669891e6185a30cfb1f76189
Arafat <arafat97alim@gmail.com>
2021-07-16 22:20:51 +0530

    PropsMethod: ChildComponent sending props to Parent.

--- src/MyComponents/PropsMethod/ChildComponent.jsx
+++ src/MyComponents/PropsMethod/ChildComponent.jsx
@@ -1,11 +1,11 @@
 //Getting Methods Props from the ParentComponent
-
+//2) now childcomponent passing the props to the parent component with the help of arrow function
 import React from "react";
 
 function ChildComponent(props) {
   return (
     <div>
-      <button onClick={props.clickHandler}>Greet Parent</button>
+      <button onClick={() => props.clickHandler("child")}>Greet Parent</button>
     </div>
   );
 }
