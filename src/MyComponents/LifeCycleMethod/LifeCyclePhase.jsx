af171419c3652314f5d0442a05c61c06af7baf43
Arafat <arafat97alim@gmail.com>
2021-07-17 22:01:19 +0530

    LifeCyle Method - Both mounting + updating.

--- src/MyComponents/LifeCycleMethod/LifeCyclePhase.jsx
+++ src/MyComponents/LifeCycleMethod/LifeCyclePhase.jsx
@@ -12,18 +12,42 @@ export default class LifeCyclePhase extends Component {
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log("LifeCyle - Mouting Phase - getDerivedStateFromProps");
+    console.log(
+      "LifeCyle - 1st Mouting Phase , 2nd Updating Phase - getDerivedStateFromProps"
+    );
+
     return null;
   }
 
   componentDidMount() {
     console.log("LifeCyle - Mouting Phase - ComponentDidMount");
   }
+
+  shouldComponentUpdate() {
+    console.log("LifeCycle - Updating Phase - shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProp, prevState) {
+    console.log("LifeCycle - Updating Phase - getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("Lifecycle - Updating Phase - componentDidUpdate");
+  }
+
+  changeState = () => {
+    this.setState({
+      message: "Change state",
+    });
+  };
   render() {
-    console.log("LifeCyle - Mouting Phase - render");
+    console.log("LifeCyle - 1st Mouting Phase , 2nd Updating Phase - render");
     return (
       <div>
         <h1>LifeCycle Phase - Parent Child</h1>
+        <button onClick={this.changeState}>Click Me</button>
         {<LifeCyclePhaseChild />}
       </div>
     );
--- src/MyComponents/LifeCycleMethod/LifeCyclePhaseChild.jsx
+++ src/MyComponents/LifeCycleMethod/LifeCyclePhaseChild.jsx
@@ -11,15 +11,33 @@ export default class LifeCyclePhaseChild extends Component {
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log("LifeCyleChild - Mouting Phase - getDerivedStateFromProps");
+    console.log(
+      "LifeCyleChild - 1st Mouting Phase , 2nd Updating Phase - getDerivedStateFromProps"
+    );
     return null;
   }
 
   componentDidMount() {
     console.log("LifeCyleChild - Mouting Phase - ComponentDidMount");
   }
+
+  shouldComponentUpdate() {
+    console.log("LifeCyleChild - Updating Phase - shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProp, prevState) {
+    console.log("LifeCyleChild - Updating Phase - getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifeCyleChild - Updating Phase - componentDidUpdate");
+  }
   render() {
-    console.log("LifeCyleChild - Mouting Phase - render");
+    console.log(
+      "LifeCyleChild - 1st Mouting Phase , 2nd Updating Phase - render"
+    );
     return (
       <div>
         <h1>LifeCycle Phase - Child</h1>
